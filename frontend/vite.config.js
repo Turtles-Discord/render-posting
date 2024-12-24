cd2caafd3a4f8a79113aa215097109e56ae68af1
buyyaa <buyya32659@gmail.com>
2024-12-24 13:37:55 -0800

    moved the viteconfig file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,18 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      // Exclude storybook stories
+      exclude: /\.stories\.(t|j)sx?$/,
+      // Only .jsx and .tsx files
+      include: "**/*.{jsx,tsx}",
+      // babel options
+      babel: {
+        plugins: ['@babel/plugin-transform-react-jsx'],
+      },
+    }),
+  ],
   base: '/',
   server: {
     port: 5173,
