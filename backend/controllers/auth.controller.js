aae7d7a390b633409e6367e52f0bb6b1717e9abc
buyyaa <buyya32659@gmail.com>
2025-01-05 14:20:53 -0800

    added JWT token and also consloe log part 2

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -200,17 +200,3 @@ export const checkAuth = async (req, res) => {
 		res.status(400).json({ success: false, message: error.message });
 	}
 };
-
-const generateTokenAndSetCookie = (res, userId) => {
-	const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
-		expiresIn: "7d",
-		algorithm: "HS256"  // Explicitly set the algorithm
-	});
-
-	res.cookie("token", token, {
-		httpOnly: true,
-		secure: process.env.NODE_ENV === "production",
-		sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
-		maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
-	});
-};
